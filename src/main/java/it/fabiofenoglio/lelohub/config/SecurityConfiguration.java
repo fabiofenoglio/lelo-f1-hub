a9ea27671f33dc495e61f53c9759d21ed395f9ff
Fabio Fenoglio <fabio.fenoglio@gmail.com>
2020-05-21 22:44:16 +0200

    added COLL profile

--- src/main/java/it/fabiofenoglio/lelohub/config/SecurityConfiguration.java
+++ src/main/java/it/fabiofenoglio/lelohub/config/SecurityConfiguration.java
@@ -77,6 +77,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         .and()
             .authorizeRequests()
+            .antMatchers("/api/status").permitAll()
             .antMatchers("/api/authenticate").permitAll()
             .antMatchers("/api/register").permitAll()
             .antMatchers("/api/activate").permitAll()
